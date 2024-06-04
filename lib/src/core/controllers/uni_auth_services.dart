dd93bc328ca09a184f7ac21fbc82a2392f87516d
Abdullah Ahmed <82912311+abdallah128128@users.noreply.github.com>
2024-06-04 17:50:34 +0300

    remove unused import

--- lib/src/core/controllers/uni_auth_services.dart
+++ lib/src/core/controllers/uni_auth_services.dart
@@ -3,7 +3,6 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:uni_auth/src/core/controllers/auth_error_handling.dart';
 import 'package:uni_auth/src/core/controllers/email_controller.dart';
-import 'package:uni_auth/src/core/controllers/unifonic_controller.dart';
 import 'package:uni_auth/src/network/server_calls.dart';
 import 'package:uni_auth/uni_auth.dart';
 
